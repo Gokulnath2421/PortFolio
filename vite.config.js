c78f134ed9d4853b71d0d8005022ad2f45ec7cb3
Your Name <you@example.com>
2025-07-06 20:00:32 +0530

    Update README with project overview, instructions, and tech stack

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   base: '/Portfolio/',  // ⬅️ if your repo is github.com/Akashh0/Portfolio/Resume
