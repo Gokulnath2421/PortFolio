59e5ef292afed1d1346720c8ac5e625e7307ce3a
Akashh0 <akashkrishh75@gmail.com>
2025-07-13 18:02:00 +0530

    Modify

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AboutMe.css';
-import profileImage from "Images\Camera Roll 1\WhatsApp Image 2025-04-29 at 20.03.45_1b6c387f.jpg" // Replace with your image path
+import profileImage from "\Assets\My Photo.jpg" // Replace with your image path
 
 export default function AboutMe() {
   return (
